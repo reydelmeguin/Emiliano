15d5d1b5046f3d7b37999ffd458c88da53bd6c89
reldelmeguin <ereveloa2200@alumno.ipn.mx>
2024-04-14 16:18:34 -0600

    algoritmiaproblemas

---
+++ problemasalgoritmia/algoritmiajs.js
@@ -0,0 +1,119 @@
+
+
+
+function problema1(){
+
+    var p1_input = document.querySelector('#p1-input').value;
+
+
+
+    //tenemos que detectar el espacio, tendria que dividir la cadena y colocarla
+    //dentro de un array
+
+    var p1_array = p1_input.split(' ').reverse();
+
+
+
+      //dentro de un array
+      //dentrodeunarray
+      //alreves
+      //separar
+
+    var p1_res = '';
+
+
+    p1_array.forEach(function (palabra, i){
+        if(i != 0 || i != p1_array.length) p1_res += ' ';
+        p1_res += palabra;
+    });
+
+    document.querySelector('#p1-output').textContent = p1_res;
+
+}
+
+function problema2(){
+    var p2_x1 = document.querySelector('#p2-x1').value;
+    var p2_x2 = document.querySelector('#p2-x2').value;
+    var p2_x3 = document.querySelector('#p2-x3').value;
+    var p2_x4 = document.querySelector('#p2-x4').value;
+    var p2_x5 = document.querySelector('#p2-x5').value;
+
+    var p2_y1 = document.querySelector('#p2-y1').value;
+    var p2_y2 = document.querySelector('#p2-y2').value;
+    var p2_y3 = document.querySelector('#p2-y3').value;
+    var p2_y4 = document.querySelector('#p2-y4').value;
+    var p2_y5 = document.querySelector('#p2-y5').value;
+
+    //vamos a formar los vectores
+    var v1 = [p2_x1, p2_x2, p2_x3, p2_x4, p2_x5];
+    var v2 = [p2_y1, p2_y2, p2_y3, p2_y4, p2_y5];
+
+    //vamos a meter una funcion de ordenamiento
+    v1 = v1.sort(function(a,b){
+        return b-a;
+    });
+    v2 = v2.sort(function(a,b){
+        return b-a;
+    });
+
+    //invierto la cadena
+    v2 = v2.reverse();
+
+    //vamos a recorrer el v1 y lo vamos a multiplicar el inverso del v2
+    var p2_producto = 0;
+    for(var i = 0; i < v1.length; i++){
+        p2_producto += v1[i] * v2[i];
+    }
+    //muestro el resultado
+    document.querySelector('#p2-output').textContent = 'El producto escalar minimo es de: ' + p2_producto;
+}
+
+function problema3(){
+    //definir un albeto
+    var alfabeto = ['A', 'B', 'C', 'D', 'E', 'F',
+            'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z'];
+
+    //Obtener la cadena del input y separar por comas
+    
+    var p3_input = document.querySelector('#p3-input').value;
+
+    var p3_palabras = p3_input.split(',');
+
+    //eliminar el espacio que hay entre cada palabra
+    p3_palabras = p3_palabras.map(function (palabra){
+        return palabra.replace(/\s/g, '').toUpperCase();
+    });
+
+    //calcular los caracteres unicos de cada palabra
+
+    var p3_res = '';
+
+    //iterar en cada palabra
+
+    p3_palabras.forEach(function (palabra, i){
+        //separar las palabras en un array para leer cada letra
+        var letras_unicas = [];
+        var palabra_array = palabra.split('');
+        //iterar al alfabeto
+        alfabeto.forEach(function (letra, j){
+            //iterar por palabra
+            palabra_array.forEach(function (letra_palabra, k){
+                //comprobar que la letra esta dentro del alfabeto
+                if(letra_palabra == letra){
+                    //si la letra no la hemos contado, la agregamos a un array
+                    //para contar las letras unicas
+                    if(letras_unicas.indexOf(letra) < 0){
+                        letras_unicas.push(letra);
+                    }
+                }
+            });
+
+        });
+        p3_res += 'Palabra: ' + palabra + ' = ' + letras_unicas.length + '\n';
+    });
+
+    document.querySelector('#p3-output').textContent = p3_res;
+
+}
